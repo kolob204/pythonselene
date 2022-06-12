589e0096adffaff639a88d1f90e056868ddd86de
viktor <elastic@yandex.ru>
2022-06-12 21:11:15 +0400

    add init vatiants of driver and options
    * Clear Selenium
    * selene

--- test3.py
+++ test3.py
@@ -1,43 +1,18 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.wait import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
+from selene.api import *
+from selene.browsers import BrowserName
 
-from selenium.webdriver.support import expected_conditions as ec
+# #some - id
+# .some - class
+#Configuration
 
 url = "https://www.google.com/"
+#config.browser_name = BrowserName.FIREFOX
+config.browser_name = BrowserName.CHROME
+#TimeOuts:
+config.timeout = 10
 
-chrome_options = webdriver.ChromeOptions()
-#chrome_options.add_argument('--headless')
-chrome_options.add_argument('-start-maximized')
+#===========================================================
+browser.open_url(url)
 
-experimentalFlags = ['same-site-by-default-cookies@1','cookies-without-same-site-must-be-secure@1']
-chromeLocalStatePrefs = { 'browser.enabled_labs_experiments' : experimentalFlags}
-chrome_options.add_experimental_option('localState',chromeLocalStatePrefs)
-
-driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(),options=chrome_options)
-driver.implicitly_wait(15)
-#=====================================================
-
-driver.get(url)
-element1 = driver.find_element_by_css_selector('[name="q"]')
-element1.send_keys('selene')
-element1.send_keys(Keys.ENTER)
-
-wait = WebDriverWait(driver, 15)
-
-wait.until(ec.visibility_of_element_located((By.CSS_SELECTOR, '#search')))
-element = driver.find_element_by_css_selector('[id="search"]')
-#print(element.text)
-wait.until(ec.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'Selene - User-oriented Web UI browser tests in Python')]")))
-
-driver.quit()
-
-
-
-
-
-
-#assert element.text  'Selene - User-oriented Web UI browser tests in Python'
-#StringAssert.Contains(expected, element.text, "Assert error!!");
+browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
